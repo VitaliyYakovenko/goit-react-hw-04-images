b6d701ec54d493d0462e0c9955f29aa8abfc00f7
VitaliyYakovenko <yakovenkovitaly1@gmail.com>
2023-03-23 17:30:44 +0200

    add commit - 4

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -1,95 +1,95 @@
-import {useEffect} from "react";
-import { createPortal } from "react-dom";
-import PropTypes from 'prop-types';
-import css from "./Modal.module.css"
-const modalRoot = document.getElementById("modal-root");
-
-
-export default function Modal({modalImg, closeModal}) {
-
-    useEffect(() => {
-        window.addEventListener("keydown", hendleKeydown);
-       
-        return (() => window.removeEventListener("keydown", hendleKeydown)); 
-    },[hendleKeydown])
-
-    const hendleKeydown = (e) => {
-        if (e.code === "Escape") {
-        closeModal();
-        }
-    } 
-    
-    const hendBackropClick = (e) => {
-        if (e.currentTarget === e.target) {
-           closeModal()
-       }
-    }
-  
-    {
-        return createPortal(<div onClick={hendBackropClick} className={css.overlay}>
-            <div className={css.modal}>
-                <img src={modalImg} data-img={modalImg} alt="modal-img" width="800" />
-            </div>
-        </div>, modalRoot);
-        
-    }
-    
-}
-
-
-Modal.propTypes = {
-        modalImg: PropTypes.string.isRequired,
-        closeModal: PropTypes.func.isRequired,
-    }
-
-
-
-// import React, { Component } from "react";
+// import {useEffect} from "react";
 // import { createPortal } from "react-dom";
 // import PropTypes from 'prop-types';
 // import css from "./Modal.module.css"
-
 // const modalRoot = document.getElementById("modal-root");
 
 
-// class Modal extends Component {
-     
-//     static propTypes = {
-//         modalImg: PropTypes.string.isRequired,
-//         closeModal: PropTypes.func.isRequired,
-//     }
-    
-//     componentDidMount() {
-//         window.addEventListener("keydown", this.hendleKeydown)
-//     }
-//     componentWillUnmount() {
-//         window.removeEventListener("keydown", this.hendleKeydown)
-//     }
+// export default function Modal({modalImg, closeModal}) {
 
-//     hendleKeydown = (e) => {
+//     useEffect(() => {
+//         window.addEventListener("keydown", hendleKeydown);
+       
+//         return (() => window.removeEventListener("keydown", hendleKeydown)); 
+//     },[modalImg])
+
+//     const hendleKeydown = (e) => {
 //         if (e.code === "Escape") {
-//         this.props.closeModal();
+//         closeModal();
 //         }
 //     } 
     
-//     hendBackropClick = (e) => {
+//     const hendBackropClick = (e) => {
 //         if (e.currentTarget === e.target) {
-//            this.props.closeModal()
+//            closeModal()
 //        }
 //     }
   
-//     render()  {
-//         return createPortal(<div onClick={this.hendBackropClick} className={css.overlay}>
+//     {
+//         return createPortal(<div onClick={hendBackropClick} className={css.overlay}>
 //             <div className={css.modal}>
-//                 <img src={this.props.modalImg} data-img={this.props.modalImg} alt="modal-img" width="800" />
+//                 <img src={modalImg} data-img={modalImg} alt="modal-img" width="800" />
 //             </div>
 //         </div>, modalRoot);
         
-//         }
+//     }
+    
+// }
+
+
+// Modal.propTypes = {
+//         modalImg: PropTypes.string.isRequired,
+//         closeModal: PropTypes.func.isRequired,
 //     }
 
 
-// export default Modal;
+
+import React, { Component } from "react";
+import { createPortal } from "react-dom";
+import PropTypes from 'prop-types';
+import css from "./Modal.module.css"
+
+const modalRoot = document.getElementById("modal-root");
+
+
+class Modal extends Component {
+     
+    static propTypes = {
+        modalImg: PropTypes.string.isRequired,
+        closeModal: PropTypes.func.isRequired,
+    }
+    
+    componentDidMount() {
+        window.addEventListener("keydown", this.hendleKeydown)
+    }
+    componentWillUnmount() {
+        window.removeEventListener("keydown", this.hendleKeydown)
+    }
+
+    hendleKeydown = (e) => {
+        if (e.code === "Escape") {
+        this.props.closeModal();
+        }
+    } 
+    
+    hendBackropClick = (e) => {
+        if (e.currentTarget === e.target) {
+           this.props.closeModal()
+       }
+    }
+  
+    render()  {
+        return createPortal(<div onClick={this.hendBackropClick} className={css.overlay}>
+            <div className={css.modal}>
+                <img src={this.props.modalImg} data-img={this.props.modalImg} alt="modal-img" width="800" />
+            </div>
+        </div>, modalRoot);
+        
+        }
+    }
+
+
+export default Modal;
 
 
 
